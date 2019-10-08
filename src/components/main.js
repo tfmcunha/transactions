0fb14d3c02941df84933a664b8f08037e242ccab
Tiago Cunha <tcunha_lp@hotmail.com>
2019-10-08 15:09:48 +0100

    added bootstrap

--- src/components/main.js
+++ src/components/main.js
@@ -1,25 +1,38 @@
-import React, {useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 
 
-export default function Main(){
+export default function Main(){	
 
-	const Username = "code-challenge"
-	const Password = "payvisioner"
-	const header = new Headers()	
-	header.set('Authorization', 'Basic ' + btoa(Username + ":" + Password)) 
+	const [action, setAction] = useState("")
+	const [currencyCode, setCurrencyCode] = useState("")
 
-	useEffect(() => {
-		async function getTransactions(){
-			const res = await fetch('https://jovs5zmau3.execute-api.eu-west-1.amazonaws.com/prod/transactions',{
-				method: "GET",
-				headers: header
-			})
-			const data = await res.json()
-			console.log(data)
-		}
-		getTransactions()
-	}, [])
-	return(
-		<div>Hello</div>
+	async function getTransactions(){
+		const Username = "code-challenge"
+		const Password = "payvisioner"
+		const header = new Headers()	
+		header.set('Authorization', 'Basic ' + btoa(Username + ":" + Password)) 
+
+		const res = await fetch(`https://jovs5zmau3.execute-api.eu-west-1.amazonaws.com/prod/transactions${action+currencyCode}`,{
+			method: "GET",
+			headers: header
+		})
+		const data = await res.json()
+	}
+	
+	return(		
+		<>
+			<div class="dropdown">
+			  <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown">
+			    Transaction Type
+			  </button>
+			  <div class="dropdown-menu" >
+			    <button class="dropdown-item">Payment</button>
+			    <button class="dropdown-item">Credit</button>
+			    <button class="dropdown-item">Authorize</button>
+			  </div>
+			</div>
+
+			<button onClick={getTransactions}>Search</button>
+		</>
 	);
 }
